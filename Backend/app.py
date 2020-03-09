2cf1556dc84ee37ad101eb4058de9d1b3c2dd872
Javit96 <javitmoreno924@gmail.com>
2020-03-08 21:52:35 -0300

    Backend User

--- Backend/app.py
+++ Backend/app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from resources.Register import Register
 from resources.Signin import Signin
 from resources.task import Tasks
+from resources.user import UserInfo
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -13,3 +14,5 @@ api.add_resource(Register, '/register')
 api.add_resource(Signin, "/singin")
 
 api.add_resource(Tasks, "/tasks")
+
+api.add_resource(UserInfo, "/info")
--- Backend/resources/user.py
+++ Backend/resources/user.py
@@ -1,1 +1,24 @@
 from flask_restful import Resource
+from flask import request
+from model import db, User
+
+class UserInfo(Resource):
+    def get(self):
+        result = ""
+        json_data = request.get_json(force = True)
+        header = request.headers["Authorization"]
+
+        if not header:
+            print("No apiKey")
+            return{"Message": 'No apiKey'}, 400
+
+        else:
+            user = User.query.filter_by(api_key = header).first()
+
+            if user:
+                result = User.serialize(user)
+            else:
+                print("Wrong api Key")
+                return{"Message": "Wrong api"},400
+
+        return{"status": 'succes', 'data': result},201
